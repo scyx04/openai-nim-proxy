66f5f311459232b535068372fda539c5b4aa0d57
scyx04 <scyx04@outlook.com>
2026-07-04 13:45:18 -0700

    Update server.js

--- server.js
+++ server.js
@@ -25,8 +25,9 @@ const ENABLE_THINKING_MODE = false; // Set to false to disable chat_template_kwa
 // Model mapping (adjust based on available NIM models)
 const MODEL_MAPPING = {
   'gpt-3.5-turbo': 'meta/llama-4-maverick-17b-128e-instruct',
-  'gpt-4':         'bytedance/seed-oss-36b-instruct',
-  'gpt-4-turbo':   'deepseek-ai/deepseek-r1',
+  //'gpt-4':         'bytedance/seed-oss-36b-instruct',
+ 'gpt-4':         'deepseek-ai/deepseek-v4-pro', 
+ 'gpt-4-turbo':   'deepseek-ai/deepseek-r1',
   //'gpt-4o': 'minimaxai/minimax-m2.7',
   'gpt-4o': 'deepseek-ai/deepseek-v4-flash',
   'claude-3-opus': 'nvidia/nemotron-3-super-120b-a12b',
