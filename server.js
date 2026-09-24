ac6036110532fd68ce0e24bf07c84f55999c0e2b
scyx04 <scyx04@outlook.com>
2026-09-23 20:26:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const MODEL_MAPPING = {
  'gpt-4-turbo':   'mistralai/mistral-small-4-119b-2603',
   //'gpt-4-turbo':   'deepseek-ai/deepseek-r1',
   //'gpt-4o': 'minimaxai/minimax-m3',
-  'gpt-4o': 'deepseek-ai/deepseek-v4-pro-0813',
+  'gpt-4o': 'deepseek-ai/deepseek-v4.1-flash',
   'claude-3-opus': 'nvidia/nemotron-3-super-120b-a12b',
   'claude-3-sonnet':'mistralai/mistral-small-4-119b-2603',
   'gemini-pro':    'bytedance/seed-oss-36b-instruct'
